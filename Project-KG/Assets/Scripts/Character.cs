93fcc2578c4a73b1c11534fe8f77c2800e676b3b
Alex <alekseyka221@gmail.com>
2020-06-23 19:01:15 +0200

    v.0.0.2
    add Animations idle, run, jump

--- Project-KG/Assets/Scripts/Character.cs
+++ Project-KG/Assets/Scripts/Character.cs
@@ -11,6 +11,13 @@ public class Character : MonoBehaviour
     [SerializeField]
     private float jumpForce = 15.0F;
     private bool isGrounded = false;
+
+    private CharState State
+    {
+        get { return (CharState)animator.GetInteger("State"); }
+        set { animator.SetInteger("State", (int)value); }
+    }
+
     new private Rigidbody2D rigidbody;
     private Animator animator;
     private SpriteRenderer sprite;
@@ -26,6 +33,7 @@ public class Character : MonoBehaviour
     }
     private void Update()
     {
+        if (isGrounded) State = CharState.Idle;
         if (Input.GetButton("Horizontal")) Run();
         if (isGrounded && Input.GetButtonDown("Jump")) Jump();
     }
@@ -36,6 +44,8 @@ public class Character : MonoBehaviour
         transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speed * Time.deltaTime);
 
         sprite.flipX = direction.x < 0.0F;
+
+        if (isGrounded) State = CharState.Run;
     }
     private void Jump()
     {
@@ -46,5 +56,14 @@ public class Character : MonoBehaviour
         Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.3F);
 
         isGrounded = colliders.Length > 1;
+
+        if (!isGrounded) State = CharState.Jump;
     }
+}
+
+public enum CharState
+{
+    Idle,
+    Run,
+    Jump
 }

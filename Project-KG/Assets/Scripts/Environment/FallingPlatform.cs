df4ab7f1e5686342d12d7123338b8ab955e86e07
Alex <alekseyka221@gmail.com>
2020-06-24 10:11:06 +0200

    v.0.0.3
    add Falling and Moving Platforms. fix animation bug

---
+++ Project-KG/Assets/Scripts/Environment/FallingPlatform.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour
+{
+    new Rigidbody2D rigidbody;
+     
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        
+    }
+
+    private void OnCollisionEnter2D (Collision2D collision)
+    {
+        if(collision.gameObject.name.Equals("Character"))
+        {
+            Invoke("FallPlatform", 1f);
+            Destroy(gameObject, 1.5f);
+        }
+    }
+
+    void FallPlatform()
+    {
+        rigidbody.isKinematic = false;
+    }
+    
+}

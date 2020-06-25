10419bd97a2423cf4bf4bc8557fdc336cd6590bb
Alex <alekseyka221@gmail.com>
2020-06-25 18:47:19 +0200

    v.0.0.4(mda)
    add melee and shooting attacks

--- Project-KG/Assets/Scripts/Units/Character.cs
+++ Project-KG/Assets/Scripts/Units/Character.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Character : MonoBehaviour
+public class Character : Unit
 {
     //[SerializeField]
    // private int lives = 5;
@@ -10,6 +10,11 @@ public class Character : MonoBehaviour
     private float speed = 3.0F;
     [SerializeField]
     private float jumpForce = 15.0F;
+    [SerializeField]
+    private float dashTime;
+    [SerializeField]
+    private float dashCoolDown = 3F;
+
 
     private bool isGrounded;
     public Transform groundCheck;
@@ -17,40 +22,48 @@ public class Character : MonoBehaviour
     public LayerMask whatIsGround;
     public int dubleJump;
 
+    public Transform punch1;
+    public float punch1Radius;
 
-    private Animator animator;
+    private Bullet bullet;
+
+    // private Animator animator;
 
     new private Rigidbody2D rigidbody;
     
     private SpriteRenderer sprite;
-    private CharState State
+   /* private CharState State
     {
         get { return (CharState)animator.GetInteger("State"); }
         set { animator.SetInteger("State", (int)value); }
-    }
+    }*/
 
     private void Awake()
     {
         rigidbody = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
+        //animator = GetComponent<Animator>();
         sprite = GetComponentInChildren<SpriteRenderer>();
+        bullet = Resources.Load<Bullet>("Bullet");
     }
     private void FixedUpdate()
-    {        
+    {   
         CheckGround();
     }
     private void Update()
     {
+        dashTime += Time.deltaTime;
         if (isGrounded == true) dubleJump = 1;
- 
-        if (isGrounded) State = CharState.Idle;
+
+        if (Input.GetButtonDown("Fire1")) Shoot();
+        //if (isGrounded) State = CharState.Idle;
         if (Input.GetButton("Horizontal")) Run();
 
         if (Input.GetKeyDown(KeyCode.UpArrow) && dubleJump > 0) Jump();
-         
-        if (Input.GetKeyDown(KeyCode.LeftShift)) Dash();
 
+        if (Input.GetKeyDown(KeyCode.LeftShift))Dash();
+        if (Input.GetKeyDown(KeyCode.Space)) MelleAttack.Action(punch1.position, punch1Radius, 9, 12, false);//координата точки нанесения урона, радиус действия урона, какому слою объектовв наносим урон, количество урона, массовый ли урон
         
+
     }
     private void Run()
     {
@@ -60,27 +73,42 @@ public class Character : MonoBehaviour
 
         sprite.flipX = direction.x < 0.0F;
 
-        if (isGrounded) State = CharState.Run;
+        //if (isGrounded) State = CharState.Run;
     }
     private void Jump()
     {
+
         rigidbody.velocity = Vector2.up * jumpForce;
         dubleJump--;
     }
 
     private void Dash()
     {
-        rigidbody.constraints = RigidbodyConstraints2D.FreezePositionY;
-        if(sprite.flipX) { rigidbody.velocity = Vector2.left * jumpForce; }
-        else { rigidbody.velocity = Vector2.right * jumpForce * 0.8F;}
-        rigidbody.constraints = RigidbodyConstraints2D.None;
-        rigidbody.constraints = RigidbodyConstraints2D.FreezeRotation;
+        if(dashTime >= dashCoolDown) {
+            Vector3 direction = transform.right;
+            transform.position = Vector3.MoveTowards(transform.position, transform.position + direction* (sprite.flipX ? -1.0F : 1.0F), jumpForce);
+            /*if (sprite.flipX) { rigidbody.velocity = Vector2.left * jumpForce; }
+        else { rigidbody.velocity = Vector2.right * jumpForce * 0.8F;}*/
+        
+        dashTime = 0;
+        }
+        
     }
+
+    private void Shoot()
+    {
+        Vector3 position = transform.position; position.y += 0.8F;
+        Bullet newBullet = Instantiate(bullet, position, bullet.transform.rotation);
+
+        newBullet.Parent = gameObject;
+        newBullet.Direction = newBullet.transform.right * (sprite.flipX ? -1.0F : 1.0F);
+    }
+
     private void CheckGround()
     {
         
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
-        if (!isGrounded) State = CharState.Jump;
+        //if (!isGrounded) State = CharState.Jump;
     }
 }
 
---
+++ Project-KG/Assets/Scripts/Units/Monsters/Monsters.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monsters : MonoBehaviour
+{
+
+    public float HP = 100;
+    // Start is called before the first frame update
+    
+}
